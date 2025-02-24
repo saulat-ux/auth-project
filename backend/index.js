c8f3ebb818edf2c2ffe337fd5ee07994d2747793
Saulat Zubair <saulat.zubair@gmail.com>
2025-02-24 14:07:39 +0500

    .

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { connectDB } from "./db/connectDB.js"
+
 import dotenv from "dotenv"
+import { connectDB } from "./db/connectDB.js"
 
 import authRoutes from "./routes/auth.routes.js"
 
