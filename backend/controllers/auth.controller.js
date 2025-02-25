c8e0e56e5bf8d5d4737ed9657f7c74d1da980fec
Saulat Zubair <saulat.zubair@gmail.com>
2025-02-25 17:56:50 +0500

    mail trap

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -26,6 +26,8 @@ export const signup = async (req, res) => {
         await user.save()
         generateTokenAndSetCookie(res, user._id)
 
+        sendVerificationEmail(user.email, verificationToken)
+
         res.status(201).json({
             success: true,
             message: 'User created successfully',
