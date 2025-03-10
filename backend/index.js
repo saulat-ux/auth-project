e5bfd7d7e59e1641c7f173a49062f30a53cce7b4
Saulat Zubair <saulat.zubair@gmail.com>
2025-03-10 16:07:48 +0500

    setup scripts and one host

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import dotenv from "dotenv"
 import { connectDB } from "./db/connectDB.js"
+import path from "path"
 
 import authRoutes from "./routes/auth.routes.js"
 
@@ -11,6 +12,7 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true })) // this allows to access the server from any origin
 
@@ -18,6 +20,13 @@ app.use(express.json()) // this allows to parse incoming JSON data as req.body
 app.use(cookieParser()) // this allows to parse incoming cookies as req.cookies
 
 app.use("/api/auth", authRoutes)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB() // 123
